e913a3730d511ba49cc8fb36ebfd8e1f4243a6d7
Zekromaster <dev@zekromaster.net>
2024-08-04 11:19:43 +0200

    fix: Apply a fix to vanilla chests that avoids breaking behaviour from being able to trigger from upgrades, even though it's technically not needed for now

--- src/main/java/net/zekromaster/minecraft/ironchests/mixin/ChestMixin.java
+++ src/main/java/net/zekromaster/minecraft/ironchests/mixin/ChestMixin.java
@@ -6,12 +6,17 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import net.zekromaster.minecraft.ironchests.ChestUpgrade;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChestBlock.class)
 public class ChestMixin {
+
+    @Unique
+    private boolean midUpgrade = true;
 
     @Inject(method = "onUse", at=@At("HEAD"), cancellable = true)
     void onUseMixin(World world, int x, int y, int z, PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
@@ -19,11 +24,20 @@ public class ChestMixin {
         var hand = player.getHand();
         if (hand != null) {
             if (hand.getItem() instanceof ChestUpgrade upgrade) {
+                this.midUpgrade = true;
                 if (upgrade.upgrade(world, x, y, z, player, chest)) {
                     hand.count--;
                 }
+                this.midUpgrade = false;
                 cir.setReturnValue(true);
             }
+        }
+    }
+
+    @Inject(method = "onBreak", at=@At("HEAD"), cancellable = true)
+    void onBreakMixin(World x, int y, int z, int par4, CallbackInfo ci) {
+        if (this.midUpgrade) {
+            ci.cancel();
         }
     }
 
